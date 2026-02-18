7ad8adab541a94e93e27a90f70da2812998835b4
MI <miro.ilias@gmail.com>
2026-02-18 17:48:08 +0300

    u

---
+++ molecular-modeling/04_ase_tests/01_atomization_energy/02_thermodyn_prop/N2_thermodyn.py
@@ -0,0 +1,37 @@
+from ase.build import molecule
+from ase.calculators.emt import EMT # Example calculator
+from ase.optimize import QuasiNewton
+from ase.thermochemistry import IdealGasThermo
+from ase.vibrations import Vibrations
+
+# 1. Setup and Optimize
+atoms = molecule('N2')
+atoms.calc = EMT()
+dyn = QuasiNewton(atoms)
+dyn.run(fmax=0.01)
+
+# 2. Get Potential Energy and Vibrations
+potentialenergy = atoms.get_potential_energy()
+vib = Vibrations(atoms)
+vib.run()
+vib_energies = vib.get_energies()
+
+# print the results
+vib.summary()
+
+# 3. Thermochemistry Calculation
+thermo = IdealGasThermo(
+    vib_energies=vib_energies,
+    potentialenergy=potentialenergy,
+    atoms=atoms,
+    geometry='linear', # or 'nonlinear'
+    symmetrynumber=2,
+    spin=0
+)
+
+# 4. Get H and S at 298.15K
+H = thermo.get_enthalpy(temperature=298.15)
+S = thermo.get_entropy(temperature=298.15, pressure=101325.0)
+print(f"Enthalpy: {H} eV")
+print(f"Entropy: {S} eV/K")
+
---
+++ molecular-modeling/04_ase_tests/01_atomization_energy/02_thermodyn_prop/readme.rst
@@ -0,0 +1,7 @@
+N2 enthalpy and entrophy
+========================
+
+the enthalpy of formation of N2 moelcule is 0 eV
+absolute entropy of N2 is cca 0.001986 eV/K (Google AI)
+
+
